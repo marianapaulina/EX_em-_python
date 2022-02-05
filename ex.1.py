083289208486c8c6b135a34d76f0d4ef31566868
Gabriel Henrique Alves Dias <71276316+GabrielHenri39@users.noreply.github.com>
2022-02-05 19:01:18 -0300

    Upgrade

--- ex.1.py
+++ ex.1.py
@@ -1,4 +1,4 @@
 #1. Armazene uma frase dentro de uma variável e depois exiba a mesma na tela.
 
 nome = input('Qual é o seu nome?')
-print("O nome do usuário é {}.".format(nome))
+print(f"O nome do usuário é {nome}")
